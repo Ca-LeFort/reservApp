cb0969b357c5f9ce259062f3bb1254058b8136d6
Ca-LeFort <ca.lefort@duocuc.cl>
2024-09-05 11:30:28 -0400

    se creó el servicio del usuario

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  //acá podemos crear variables:
+  usuarios: any[] = [];
+
+  constructor() { }
+
+  //aquí vamos a crear toda nuestra lógica de programación
+  //DAO:
+  public createUsuario(usuarios:any){}
+
+  public getUsuario(rut:string){}
+
+  public getUsuarios(){}
+
+  public updateUsuario(rut:string, nuevoUsuario:any){}
+
+  public deleteUsuario(rut:string){}
+
+}
